4078b89fd771e804007a904c0513676ee180ed03
Romain Pamart <romain.pamart@yahoo.fr>
2018-08-02 22:07:18 +0200

    update (Translate): translate basic text

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Alexa Skill Space Quiz
 This is a sample quiz skill about space in java for Amazon echo.
+This project is based on the sample project of Amazon: [Here](https://github.com/alexa/skill-sample-java-quiz-game)
+
+Warning /!\: All of the text in this project is in French.
 
 ## Step 1
 Follow the official Amazon Step by Step tutorial for start the project on your Amazon account.
 
-Official link: [Here](https://github.com/alexa/skill-sample-java-quiz-game)
+Official link: [Here](https://github.com/alexa/skill-sample-java-quiz-game/blob/master/instructions/1-voice-user-interface.md)
 
 ## Step 2
 
---
+++ src/com/chimanos/spacequiz/quiz/util/QuestionUtils.java
@@ -0,0 +1,95 @@
+package com.chimanos.spacequiz.quiz.util;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
+import com.chimanos.spacequiz.quiz.model.Attributes;
+import com.chimanos.spacequiz.quiz.model.StateProperty;
+import com.chimanos.spacequiz.quiz.model.State;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+
+import static com.chimanos.spacequiz.quiz.model.Constants.START_QUIZ_MESSAGE;
+import static com.chimanos.spacequiz.quiz.model.Constants.STATES;
+
+public class QuestionUtils {
+
+    private static final Random RANDOM = new Random();
+
+    public static Optional<Response> generateQuestion(HandlerInput input) {
+        Map<String, Object> sessionAttributes = input.getAttributesManager().getSessionAttributes();
+
+        int counter = (int)sessionAttributes.get(Attributes.COUNTER_KEY);
+
+        if (counter == 0) {
+            sessionAttributes.put(Attributes.RESPONSE_KEY, START_QUIZ_MESSAGE + " ");
+        }
+
+        counter++;
+        State state = getRandomState();
+        StateProperty stateProperty = getRandomProperty();
+
+        sessionAttributes.put(Attributes.QUIZ_ITEM_KEY, state);
+        sessionAttributes.put(Attributes.QUIZ_PROPERTY_KEY, stateProperty);
+        sessionAttributes.put(Attributes.COUNTER_KEY, counter);
+
+        String question = getQuestionText(counter, stateProperty, state);
+        String speech = sessionAttributes.get(Attributes.RESPONSE_KEY) + question;
+
+        return input.getResponseBuilder()
+                .withSpeech(speech)
+                .withReprompt(question)
+                .withShouldEndSession(false)
+                .build();
+    }
+
+    public static String getQuestionText(int counter, StateProperty stateProperty, State state) {
+        if(counter == 1) {
+            return "Voici votre " + counter + "ére question.  Qu'elle est la/le " + stateProperty.getValue() + " de "  + state.getName() + "?";
+        } else {
+            return "Voici votre " + counter + "éme question.  Qu'elle est la/le " + stateProperty.getValue() + " de "  + state.getName() + "?";
+        }
+    }
+
+    public static Optional<State> getState(Map<String, Slot> slots) {
+        for (Slot slot : slots.values()) {
+            String value = slot.getValue();
+            for (StateProperty stateProperty : StateProperty.values()) {
+                Optional<State> state = STATES.stream()
+                        .filter(s -> getPropertyOfState(stateProperty, s).equals(value))
+                        .findAny();
+                if (state.isPresent()) {
+                    return state;
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static String getPropertyOfState(StateProperty stateProperty, State state) {
+        switch (stateProperty) {
+            case NAME:
+                return state.getName();
+            case ABBREVIATION:
+                return state.getAbbreviation();
+            case CAPITAL:
+                return state.getCapital();
+            case STATEHOOD_YEAR:
+                return state.getStatehoodYear();
+            case STATEHOOD_ORDER:
+                return state.getStatehoodOrder();
+        }
+        throw new IllegalStateException("Invalid stateProperty");
+    }
+
+    private static State getRandomState() {
+        return STATES.get(RANDOM.nextInt(STATES.size()));
+    }
+
+    private static StateProperty getRandomProperty() {
+        return StateProperty.values()[RANDOM.nextInt(StateProperty.values().length -1) + 1];
+    }
+
+}
